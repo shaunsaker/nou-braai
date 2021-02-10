5deb6416ab46b0c80e76fe9408b069af7a2e0207
Shaun Saker <sakershaun@gmail.com>
2021-02-10 09:04:19 +0200

    add testing

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFiles: ['<rootDir>/jest/setup.js'],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  moduleNameMapper: {
+    '\\.(svg)$': '<rootDir>/__mocks__/svgMock.js',
+  },
+};
